f07b6410a123700e98e93fc948d2da8fd5a7fa13
Sanjib Talukdar <sanjeeb.talukdar@gmail.com>
2024-05-01 21:18:08 +0530

    Fixed Issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   plugins: [react()],
   server: {
     proxy : {
--- src/main/scala/ai/diffy/repository/DifferenceResultRepository.java
+++ src/main/scala/ai/diffy/repository/DifferenceResultRepository.java
@@ -3,6 +3,8 @@ package ai.diffy.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,7 @@ import ai.diffy.analysis.DifferenceResult;
 @Component
 @Repository
 public interface DifferenceResultRepository extends JpaRepository<DifferenceResult, String>{
-    List<DifferenceResult> findByTimestampMsecBetween(Long start, Long end);
+    
+    @Query("select d from DifferenceResult d where d.timestampMsec >= :starTime and d.timestampMsec <= :endTime")
+    List<DifferenceResult> findByTimestampMsecBetween(@Param("starTime") Long start,@Param("endTime") Long end);
 }
